70e6bd208bcc2c878026359eee4633aaf0140264
tqspham <tqs.pham@gmail.com>
2026-04-28 23:21:46 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import NotesApp from '@/components/NotesApp';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <NotesApp />
+    </main>
+  );
+}
